0b6a51263268247498f7e0f6712836414cf1d45f
Khalil Ur Rehman <khalilurrehman.it@gmail.com>
2025-04-21 10:01:47 +0500

    Teaching DOM to Alex

---
+++ DOM/script.js
@@ -0,0 +1,22 @@
+// let paragraph = document.getElementById("greeting");
+// paragraph.textContent = "Hi there!";
+
+// let heading2 = document.getElementById("learning");
+// heading2.textContent = "Learning JavaScript";
+
+// get element by class name
+
+// let elements = document.getElementsByClassName("special");
+
+// console.log(elements); // returns an HTMLCollection of all elements with class "special"
+
+// elements[2].textContent = "This is the first special element";
+
+// get element by tag name
+
+// let elementtagname = document.getElementsByTagName("h2");
+// elementtagname[0].textContent = "Get element by tag name h2";
+
+// get element by query selector
+let elementquery = document.querySelectorAll("#special");
+elementquery[0].textContent = "Get element by query selector #greeting";
